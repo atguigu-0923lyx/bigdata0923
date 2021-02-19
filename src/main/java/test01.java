4a53354e0ffdf8dd13e6453d5e5f670f6983c67a
liuyuxin <lyx@atguigu.com>
2021-02-19 16:50:34 +0800

    hot-fix second commit

--- src/main/java/test01.java
+++ src/main/java/test01.java
@@ -1,6 +1,6 @@
 public class test01 {
     public static void test(){
         System.out.println("test idea_git");
-        System.out.println("test hot-fix");
+        System.out.println("test hot-fix-1");
     }
 }
